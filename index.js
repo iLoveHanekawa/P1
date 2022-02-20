e1e48d77957416efb38cf40f120fc26708550b48
Arjun Tanwar <arjunthakur900@gmail.com>
2022-02-20 21:50:29 +0530

    finished p1

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const someBtn = document.getElementById("someBtn")
 function inc() {
-    document.getElementById("count").innerText++
+    document.getElementById("count").innerHTML++;
+}
+function save() {
+    let count = document.getElementById("count").textContent
+    document.getElementById("count").textContent = 0
+    document.getElementById("pentries").textContent += count + " - "
 }
